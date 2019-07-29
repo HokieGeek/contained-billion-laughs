7c0dcf493b8438c32cf9fc8e985bed4691e94dc8
HokieGeek <andres.f.perez@gmail.com>
2019-07-29 13:46:36 -0400

    Added config to change the default host to listen on

--- src/main/java/org/sonatype/se/hacks/lolz/Main.java
+++ src/main/java/org/sonatype/se/hacks/lolz/Main.java
@@ -5,6 +5,11 @@ import ro.pippo.core.Pippo;
 public class Main {
     public static void main(String[] args) {
         Pippo pippo = new Pippo(new App());
+
+        // add routes for static content
+        pippo.addPublicResourceRoute();
+        pippo.addWebjarsResourceRoute();
+
         pippo.start();
     }
 }
